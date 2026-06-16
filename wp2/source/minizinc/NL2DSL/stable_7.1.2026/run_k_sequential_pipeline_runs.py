c1de2e33f432f1d456043ff3bc1f603a10e4eb96
Katharina Blaimschein <katharina_blaimschein@aon.at>
2026-06-16 16:52:01 +0200

    refactoring

--- wp2/source/minizinc/NL2DSL/stable_7.1.2026/run_k_sequential_pipeline_runs.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-for i in range(1, 21):
-    print(f"""----------------------------------------------------------------------------
-    Starting run {i}: """)
-    proc = subprocess.Popen([sys.executable, "tryout_pipeline.py"])
-    proc.wait()
-    proc.terminate()
